c149fccc42e602d4da94468ab0abe7375cbeabf7
Natalia Sofia Martinez Lima <natasha.fifi98@gmail.com>
2019-08-22 20:36:23 -0600

    tercer commit

---
+++ untitled/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char opcion;
+	int vector[8],vector2[8];
+	int j;
+	float media,suma;
+
+vector[j]=vector2[j];
+				printf ("Los valores del vector copia son\n");
+				for (j = 0; j < 8; j++) {
+					printf ("%4d", vector2[j]);
+				}
+}
